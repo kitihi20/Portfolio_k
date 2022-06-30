40138db2e819441a3dacd0b8bbf8f9a73962d1e9
Hibiki Kirihata <kirihatah@gmail.com>
2022-07-01 01:05:26 +0900

    画像系追加、Header使いまわし
    Headerを使いまわせるように
    画像追加
    画像のアニメーション追加

--- mainjs.js
+++ mainjs.js
@@ -1,4 +1,31 @@
 
+/*ページ内ファイル読み込みってChromeでは禁止されてたりする?*/
+function LoadHeader(rootdir)
+{
+    let el = document.getElementById("headermain");
+    el.innerHTML = `
+    <button id="hbbutton" onclick="ClickHB()">
+    <span class="hbline" id="hbline1"></span>
+    <span class="hbline" id="hbline2_1"></span>
+    <span class="hbline" id="hbline2_2"></span>
+    <span class="hbline " id="hbline3"></span>
+  </button>
+  <img src="./images/AS-MadeIconProt_small.png" width="40px">
+  <span id="headertxt">
+    Portfolio_k
+  </span>
+  <div id="hmenu">
+    <button class="hmenubox" onclick="location.href='./index.html'">Home</button>
+    <button class="hmenubox" onclick="location.href='./index.html'">About</button>
+    <h1>Works</h1>
+    <button class="hmenubox" onclick="location.href='./index.html'">NonameMech v2</button>
+    <button class="hmenubox" onclick="location.href='./index.html'">Voidgazer</button>
+    <button class="hmenubox" onclick="location.href='./index.html'">~からの脱出</button>
+    <button class="hmenubox" onclick="location.href='./index.html'">欠けた世界、駆ける</button>
+    <button class="hmenubox" onclick="location.href='./index.html'">オンラインゲーム擬き</button>
+  </div>
+    `;
+}
 
 let OpenHB = false;
 
