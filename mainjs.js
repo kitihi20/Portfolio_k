6d4c8c86950efa240657ae6b84540484ed5738a6
Hibiki Kirihata <kirihatah@gmail.com>
2022-07-02 16:35:45 +0900

    Works用ページ追加、画像周辺調整

--- mainjs.js
+++ mainjs.js
@@ -1,28 +1,30 @@
 
-/*ページ内ファイル読み込みってChromeでは禁止されてたりする?*/
+/*ページ内でサーバーのファイル読み込みってChromeでは禁止されてたりする?*/
 function LoadHeader(rootdir)
 {
     let el = document.getElementById("headermain");
     el.innerHTML = `
-    <button id="hbbutton" onclick="ClickHB()">
+    <button id="hbbutton" class="blborder" onclick="ClickHB()">
     <span class="hbline" id="hbline1"></span>
     <span class="hbline" id="hbline2_1"></span>
     <span class="hbline" id="hbline2_2"></span>
     <span class="hbline " id="hbline3"></span>
   </button>
-  <img src="./images/AS-MadeIconProt_small.png" width="40px">
-  <span id="headertxt">
-    Portfolio_k
-  </span>
+  <button class="none blborder" onclick="location.href='./index.html'">
+    <img src="./images/AS-MadeIconProt_small.png" width="40px">
+    <span id="headertxt" class="CGf">
+      Portfolio_k
+    </span>
+  </button>
   <div id="hmenu">
-    <button class="hmenubox" onclick="location.href='./index.html'">Home</button>
-    <button class="hmenubox" onclick="location.href='./index.html'">About</button>
+    <button class="hmenubox blborder" onclick="location.href='./index.html'">Home</button>
+    <button class="hmenubox blborder" onclick="location.href='./index.html'">About</button>
     <h1>Works</h1>
-    <button class="hmenubox" onclick="location.href='./index.html'">NonameMech v2</button>
-    <button class="hmenubox" onclick="location.href='./index.html'">Voidgazer</button>
-    <button class="hmenubox" onclick="location.href='./index.html'">~からの脱出</button>
-    <button class="hmenubox" onclick="location.href='./index.html'">欠けた世界、駆ける</button>
-    <button class="hmenubox" onclick="location.href='./index.html'">オンラインゲーム擬き</button>
+    <button class="hmenubox blborder" onclick="location.href='./NonameMech.html'">NonameMech v2</button>
+    <button class="hmenubox blborder" onclick="location.href='./index.html'">Voidgazer</button>
+    <button class="hmenubox blborder" onclick="location.href='./index.html'">~からの脱出</button>
+    <button class="hmenubox blborder" onclick="location.href='./index.html'">欠けた世界、駆ける</button>
+    <button class="hmenubox blborder" onclick="location.href='./index.html'">オンラインゲーム擬き</button>
   </div>
     `;
 }
@@ -30,6 +32,7 @@ function LoadHeader(rootdir)
 let OpenHB = false;
 
 function ClickHB(){
+    
     let element = document.getElementById("headermain");
     OpenHB = !OpenHB;
     if(OpenHB)
