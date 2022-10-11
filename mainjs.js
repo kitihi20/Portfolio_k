b154566f96c5401322506a6406f8bcb859721606
Hibiki Kirihata <kirihatah@gmail.com>
2022-10-11 15:59:04 +0900

    ページ追加、CSS調整等
    追加予定のページを追加
    それに合わせてindex,headerも更新
    スマートフォンでのCSSを変更、画像サイズが最小からある程度大きく、変化しないように

--- mainjs.js
+++ mainjs.js
@@ -5,29 +5,31 @@ function LoadHeader(rootdir)
     let el = document.getElementById("headermain");
     el.innerHTML = `
     <button id="hbbutton" class="blborder" onclick="ClickHB()">
-      <span class="hbline" id="hbline1"></span>
-      <span class="hbline" id="hbline2_1"></span>
-      <span class="hbline" id="hbline2_2"></span>
-      <span class="hbline " id="hbline3"></span>
-    </button>
-    <a href="./index.html">
-    <button class="none blborder">
-      <img src="./images/AS-MadeIconProt_small.png" width="40px">
-      <span id="headertxt" class="CGf">
-        kitihi20
-      </span>
-    </button>
-    </a>
-    <div id="hmenu">
-      <a href="./index.html"><button class="hmenubox blborder">Home</button></a>
-      <a href="./About.html"><button class="hmenubox blborder">About</button></a>
-      <h1>Works</h1>
-      <a href="./NonameMech.html"><button class="hmenubox blborder">NonameMech v2</button></a>
-      <a href="./Voidlase.html"><button class="hmenubox blborder">VOIDLASE</button></a>
-      <a href="./Escape.html"><button class="hmenubox blborder">〜からの脱出</button></a>
-      <a href="./Crack.html"><button class="hmenubox blborder">欠けた世界、駆ける</button></a>
-      <a href="./Onlinelike.html"><button class="hmenubox blborder">オンラインゲーム擬き</button></a>
-    </div>
+    <span class="hbline" id="hbline1"></span>
+    <span class="hbline" id="hbline2_1"></span>
+    <span class="hbline" id="hbline2_2"></span>
+    <span class="hbline " id="hbline3"></span>
+  </button>
+  <a href="./index.html">
+  <button class="none blborder">
+    <img src="./images/AS-MadeIconProt_small.png" width="40px">
+    <span id="headertxt" class="CGf">
+      kitihi20
+    </span>
+  </button>
+  </a>
+  <div id="hmenu">
+    <a href="./index.html"><button class="hmenubox blborder">Home</button></a>
+    <a href="./About.html"><button class="hmenubox blborder">About</button></a>
+    <h1>Works</h1>
+    <a href="./GTMech3.html"><button class="hmenubox blborder">GravityTechnologyMech v3.0(仮)</button></a>
+    <a href="./Voidlase.html"><button class="hmenubox blborder">VOIDLASE</button></a>
+    <a href="./NonameMech.html"><button class="hmenubox blborder">NonameMech v2</button></a>
+    <a href="./AnomalyCorrector.html"><button class="hmenubox blborder">AnomalyCorrector</button></a>
+    <a href="./Escape.html"><button class="hmenubox blborder">〜からの脱出</button></a>
+    <a href="./Crack.html"><button class="hmenubox blborder">欠けた世界、駆ける</button></a>
+    <a href="./Onlinelike.html"><button class="hmenubox blborder">オンラインゲーム擬き</button></a>
+  </div>
     `;
 }
 
